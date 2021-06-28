4a139fd87061c6eb7ca7ca7c74a760911e3ca90d
Sungchul-P <48536668+Sungchul-P@users.noreply.github.com>
2021-06-28 21:35:33 +0900

    feat: 5. Explorer(Go Template으로 서버 사이드 렌더링 웹 페이지 작성)

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 )
 
-type block struct {
+type Block struct {
 	Data     string
 	Hash     string
 	PrevHash string
@@ -15,7 +15,7 @@ type block struct {
 type blockchain struct {
 	// blocks 를 포인터의 슬라이스로 변경.
 	// 블록체인이 매우 길어질 수 있으므로 복사가 일어나지 않기 위한 것이다.
-	blocks []*block
+	blocks []*Block
 }
 
 // 패키지 내부에서만 사용 가능
@@ -24,7 +24,7 @@ var once sync.Once
 
 // calculateHash 는 block 리시버로 데이터와 직전 해시값을 조합하여 새로운 해시 값을 계산한다.
 // 해싱 알고리즘은 SHA256을 많이 사용하고, 출력 포맷은 16진수로 변형한다.
-func (b *block) calculateHash() {
+func (b *Block) calculateHash() {
 	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
 	b.Hash = fmt.Sprintf("%x", hash)
 }
@@ -39,8 +39,8 @@ func getLastHash() string {
 }
 
 // createBlock 은 새로운 블록을 만들어서 포인터를 반환한다.
-func createBlock(data string) *block {
-	newBlock := block{data, "", getLastHash()}
+func createBlock(data string) *Block {
+	newBlock := Block{data, "", getLastHash()}
 	newBlock.calculateHash()
 	return &newBlock
 }
@@ -62,6 +62,6 @@ func GetBlockchain() *blockchain {
 }
 
 // AllBlocks 는 블록체인의 모든 블록을 반환한다. (출력 용도)
-func (b *blockchain) AllBlocks() []*block {
+func (b *blockchain) AllBlocks() []*Block {
 	return b.blocks
 }
---
+++ explorer/explorer.go
@@ -0,0 +1,49 @@
+package explorer
+
+import (
+	"fmt"
+	"github.com/Sungchul-P/nori-coin/blockchain"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var templates *template.Template
+
+const (
+	port        string = ":4000"
+	templateDir string = "explorer/templates/"
+)
+
+type homeData struct {
+	// 외부에서 사용할 수 있도록 첫 글자 대문자 사용
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func Start() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
