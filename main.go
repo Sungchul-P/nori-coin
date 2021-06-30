a82f3b261b5662e580b28d6c3948efd12c185853
Sungchul-P <48536668+Sungchul-P@users.noreply.github.com>
2021-06-30 17:36:36 +0900

    feat: 7.1 Parsing Commands

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"github.com/Sungchul-P/nori-coin/explorer"
-	"github.com/Sungchul-P/nori-coin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcom to 노리 코인\n")
+	fmt.Printf("Pleas use the following commands:\n")
+	fmt.Printf("explorer:		Start the HTML Explorer\n")
+	fmt.Printf("rest:			Start the REST API (recommended)\n")
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(3000)
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
+
 }
