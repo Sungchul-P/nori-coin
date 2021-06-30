5fdbc50d873bf3a26f93e7aeb9e067738be8995a
Sungchul-P <48536668+Sungchul-P@users.noreply.github.com>
2021-06-30 11:10:51 +0900

    refactor: 6.4 NewServeMux

---
+++ rest/rest.go
@@ -0,0 +1,77 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Sungchul-P/nori-coin/blockchain"
+	"github.com/Sungchul-P/nori-coin/utils"
+	"log"
+	"net/http"
+)
+
+var port string
+
+type url string
+
+func (u url) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+type urlDescription struct {
+	URL         url    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+type addBlockBody struct {
+	Message string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []urlDescription{
+		{
+			URL:         url("/"),
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+		{
+			URL:         url("/blocks/{id}"),
+			Method:      "GET",
+			Description: "See A Block",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	err := json.NewEncoder(rw).Encode(data)
+	utils.HandleErr(err)
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		utils.HandleErr(json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks()))
+	case "POST":
+		// {"data": "my block data"}
+		var addBlockBody addBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func Start(aPort int) {
+	handler := http.NewServeMux()
+	port = fmt.Sprintf(":%d", aPort)
+	handler.HandleFunc("/", documentation)
+	handler.HandleFunc("/blocks", blocks)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, handler))
+}
