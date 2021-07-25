4e3f92101253ade3f74b8146dd69ebde55b7dc09
Sungchul-P <48536668+Sungchul-P@users.noreply.github.com>
2021-07-25 19:39:14 +0900

    feat: 11.5 Wallet Backend

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,46 +2,27 @@ package wallet
 
 import (
 	"crypto/ecdsa"
-	"crypto/x509"
-	"encoding/hex"
-	"fmt"
-	"github.com/Sungchul-P/nori-coin/utils"
-	"math/big"
+	"os"
 )
 
-const (
-	signature     string = "9239255424EAE14D259D4EA25C0A2F87CB59F521765412236DA8BE2DA50609DE6B7775B1560878B165535528CD04127DF6573A3AB4313FD14B6418682D513B46"
-	privateKey    string = "307702010104209229cf3b6b67587cd4fadc354a7bf1de6dbe94ea286f960f2ce43b8aa96f1530a00a06082a8648ce3d030107a14403420004ef1d02ecfb16e1e28e11f6fb1d5fd3366ac31004316e4431caa66bd7d2ae20078a7cd9b86f397cc386f405350bac1093401196747ed12fb89759aeba52dccef9"
-	hashedMessage string = "a60a187658ba11c6022486f606015c968d4d293996737c0c6620223f83e61faf"
-)
+type wallet struct {
+	privateKey *ecdsa.PrivateKey
+}
 
-func Start() {
+var w *wallet
 
-	privBytes, err := hex.DecodeString(privateKey)
-	utils.HandleErr(err)
+func hasWalletFile() bool {
+	_, err := os.Stat("noricoin.wallet")
+	return !os.IsNotExist(err)
+}
 
-	retoredKey, err := x509.ParseECPrivateKey(privBytes)
-	utils.HandleErr(err)
+func Wallet() *wallet {
+	if w == nil {
+		if hasWalletFile() {
 
-	fmt.Println(retoredKey)
-	fmt.Println()
-	sigBytes, err := hex.DecodeString(signature)
-	rBytes := sigBytes[:len(sigBytes)/2]
-	sBytes := sigBytes[len(sigBytes)/2:]
-	fmt.Printf("%d\n\n%d\n\n%d\n\n", sigBytes, rBytes, sBytes)
+		} else {
 
-	var bigR, bigS = big.Int{}, big.Int{}
-
-	bigR.SetBytes(rBytes)
-	bigS.SetBytes(sBytes)
-
-	fmt.Println()
-	fmt.Println(bigR, bigS)
-
-	hashBytes, err := hex.DecodeString(hashedMessage)
-	utils.HandleErr(err)
-
-	ok := ecdsa.Verify(&retoredKey.PublicKey, hashBytes, &bigR, &bigS)
-	fmt.Println()
-	fmt.Println(ok)
+		}
+	}
+	return w
 }
