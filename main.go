c24c187e44ee868487e9ce0bb345685968e12d73
Sungchul-P <48536668+Sungchul-P@users.noreply.github.com>
2021-07-25 19:58:26 +0900

    feat: 11.6 Persist Wallet

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import "github.com/Sungchul-P/nori-coin/wallet"
 func main() {
 	//defer db.Close()
 	//cli.Start()
-	wallet.Start()
+	wallet.Wallet()
 }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,7 +2,15 @@ package wallet
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"github.com/Sungchul-P/nori-coin/utils"
 	"os"
+)
+
+const (
+	fileName string = "noricoin.wallet"
 )
 
 type wallet struct {
@@ -12,16 +20,32 @@ type wallet struct {
 var w *wallet
 
 func hasWalletFile() bool {
-	_, err := os.Stat("noricoin.wallet")
+	_, err := os.Stat(fileName)
 	return !os.IsNotExist(err)
+}
+
+func createPrivKey() *ecdsa.PrivateKey {
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	utils.HandleErr(err)
+	return privKey
+}
+
+func persistKey(key *ecdsa.PrivateKey) {
+	bytes, err := x509.MarshalECPrivateKey(key)
+	utils.HandleErr(err)
+	err = os.WriteFile(fileName, bytes, 0644)
+	utils.HandleErr(err)
 }
 
 func Wallet() *wallet {
 	if w == nil {
+		w = &wallet{}
 		if hasWalletFile() {
 
 		} else {
-
+			key := createPrivKey()
+			persistKey(key)
+			w.privateKey = key
 		}
 	}
 	return w
