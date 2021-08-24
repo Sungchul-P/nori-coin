694bbdcae8ddd939b1f84bc01c01e12a967825c4
Sungchul-P <48536668+Sungchul-P@users.noreply.github.com>
2021-08-25 00:00:40 +0900

    feat: 12.16 ~ 17 Data Race & Mutex

--- p2p/peer.go
+++ p2p/peer.go
@@ -3,9 +3,17 @@ package p2p
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
+	"sync"
 )
 
-var Peers map[string]*peer = make(map[string]*peer)
+type peers struct {
+	v map[string]*peer
+	m sync.Mutex
+}
+
+var Peers peers = peers{
+	v: make(map[string]*peer),
+}
 
 type peer struct {
 	key     string
@@ -15,9 +23,22 @@ type peer struct {
 	inbox   chan []byte
 }
 
+func AllPeers(p *peers) []string {
+	p.m.Lock()
+	defer p.m.Unlock()
+	var keys []string
+
+	for key := range p.v {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
 func (p *peer) close() {
+	Peers.m.Lock()
+	defer Peers.m.Unlock()
 	p.conn.Close()
-	delete(Peers, p.key)
+	delete(Peers.v, p.key)
 }
 
 func (p *peer) read() {
@@ -53,6 +74,6 @@ func initPeer(conn *websocket.Conn, address, port string) *peer {
 	}
 	go p.read()
 	go p.write()
-	Peers[key] = p
+	Peers.v[key] = p
 	return p
 }
